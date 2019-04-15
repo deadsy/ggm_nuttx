14a027d27c6e5fc1f846b2272779aeb67df06487
Jason Harris <sirmanlypowers@gmail.com>
2019-04-15 08:38:22 -0700

    WIP. rei2c driver.

--- files/nuttx/configs/axoloti/src/rei2c.c
+++ files/nuttx/configs/axoloti/src/rei2c.c
@@ -19,6 +19,17 @@
 
 //-----------------------------------------------------------------------------
 
+static const struct rei2c_cfg config[] = {
+	{REI2C_GCONF, REI2C_GCONF_ETYPE},
+	{REI2C_CVAL, 0},	// Counter Value
+	{REI2C_CMAX, 32},	// Counter Max value
+	{REI2C_CMIN, (uint32_t) - 32},	// Counter Min value
+	{REI2C_ISTEP, 1},	// Increment step value
+	{0xff, 0},		// end-of-list
+};
+
+//-----------------------------------------------------------------------------
+
 #define REI2C_I2C_PORTNO 1	// On I2C1
 
 int rei2c_initialize(char *devname) {
@@ -32,7 +43,7 @@ int rei2c_initialize(char *devname) {
 		return -ENODEV;
 	}
 	// Register the rei2c device
-	return rei2c_register(devname, i2c);
+	return rei2c_register(devname, i2c, config);
 }
 
 //-----------------------------------------------------------------------------
