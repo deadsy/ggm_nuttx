160c2e1ce98be84eb22a170e9fc58e8f9fc1836c
Jason Harris <sirmanlypowers@gmail.com>
2019-05-01 18:44:13 -0700

    Code formatting.

--- files/nuttx/drivers/input/rei2c.c
+++ files/nuttx/drivers/input/rei2c.c
@@ -1,13 +1,11 @@
 /****************************************************************************
  * drivers/input/rei2c.c
+ * I2C Rotary Encoder V2 Driver
+ * https://github.com/Fattoresaimon/I2CEncoderV2
+ * https://www.kickstarter.com/projects/1351830006/i2c-encoder-v2
  *
  *   Copyright (C) 2019 Gregory Nutt. All rights reserved.
  *   Author: Jason T Harris <sirmnalypowers@gmail.com>
- *
- * I2C Rotary Encoder V2 Driver
- * References:
- * https://github.com/Fattoresaimon/I2CEncoderV2
- * https://www.kickstarter.com/projects/1351830006/i2c-encoder-v2
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -48,320 +46,367 @@
 
 /****************************************************************************/
 
-struct rei2c_dev_s {
-	struct i2c_master_s *i2c;	// I2C interface connected to rei2c
-	sem_t sem;		// Supports exclusive access to the device
-	uint32_t speed;		// i2c bus speed
-	uint8_t addr;		// i2c device address
-	uint8_t crefs;		// Number of times the device has been opened
+struct rei2c_dev_s
+{
+  struct i2c_master_s *i2c;     // I2C interface connected to rei2c
+  sem_t sem;                    // Supports exclusive access to the device
+  uint32_t speed;               // i2c bus speed
+  uint8_t addr;                 // i2c device address
+  uint8_t crefs;                // Number of times the device has been opened
 };
 
 /****************************************************************************/
 // basic read/write functions
 
-static int rei2c_i2c_read(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * data, size_t len) {
-	struct i2c_msg_s msgv[2] = {
-		{
-		 .frequency = dev->speed,
-		 .addr = dev->addr,
-		 .flags = 0,
-		 .buffer = &reg,
-		 .length = 1,
-		 },
-		{
-		 .frequency = dev->speed,
-		 .addr = dev->addr,
-		 .flags = I2C_M_READ,
-		 .buffer = data,
-		 .length = len,
-		 }
-	};
-	int rc = I2C_TRANSFER(dev->i2c, msgv, 2);
-	if (rc < 0) {
-		ierr("I2C_TRANSFER failed %d\n", rc);
-		return rc;
-	}
-	return OK;
+static int rei2c_i2c_read(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * data,
+                          size_t len)
+{
+  struct i2c_msg_s msgv[2] = {
+    {
+     .frequency = dev->speed,
+     .addr = dev->addr,
+     .flags = 0,
+     .buffer = &reg,
+     .length = 1,
+     },
+    {
+     .frequency = dev->speed,
+     .addr = dev->addr,
+     .flags = I2C_M_READ,
+     .buffer = data,
+     .length = len,
+     }
+  };
+  int rc = I2C_TRANSFER(dev->i2c, msgv, 2);
+  if (rc < 0)
+    {
+      ierr("I2C_TRANSFER failed %d\n", rc);
+      return rc;
+    }
+  return OK;
 }
 
-static int rei2c_i2c_write(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * data, size_t len) {
-	struct i2c_msg_s msgv[2] = {
-		{
-		 .frequency = dev->speed,
-		 .addr = dev->addr,
-		 .flags = 0,
-		 .buffer = &reg,
-		 .length = 1,
-		 },
-		{
-		 .frequency = dev->speed,
-		 .addr = dev->addr,
-		 .flags = I2C_M_NOSTART,
-		 .buffer = data,
-		 .length = len,
-		 }
-	};
-	int rc = I2C_TRANSFER(dev->i2c, msgv, 2);
-	if (rc < 0) {
-		ierr("I2C_TRANSFER failed %d\n", rc);
-		return rc;
-	}
-	return OK;
+static int rei2c_i2c_write(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * data,
+                           size_t len)
+{
+  struct i2c_msg_s msgv[2] = {
+    {
+     .frequency = dev->speed,
+     .addr = dev->addr,
+     .flags = 0,
+     .buffer = &reg,
+     .length = 1,
+     },
+    {
+     .frequency = dev->speed,
+     .addr = dev->addr,
+     .flags = I2C_M_NOSTART,
+     .buffer = data,
+     .length = len,
+     }
+  };
+  int rc = I2C_TRANSFER(dev->i2c, msgv, 2);
+  if (rc < 0)
+    {
+      ierr("I2C_TRANSFER failed %d\n", rc);
+      return rc;
+    }
+  return OK;
 }
 
-static int rei2c_rd8(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * val) {
-	return rei2c_i2c_read(dev, reg, val, 1);
+static int rei2c_rd8(struct rei2c_dev_s *dev, uint8_t reg, uint8_t * val)
+{
+  return rei2c_i2c_read(dev, reg, val, 1);
 }
 
-static int rei2c_rd24(struct rei2c_dev_s *dev, uint8_t reg, uint32_t * val) {
-	return rei2c_i2c_read(dev, reg, (uint8_t *) val, 3);
+static int rei2c_rd24(struct rei2c_dev_s *dev, uint8_t reg, uint32_t * val)
+{
+  return rei2c_i2c_read(dev, reg, (uint8_t *) val, 3);
 }
 
-static int rei2c_rd32(struct rei2c_dev_s *dev, uint8_t reg, uint32_t * val) {
-	return rei2c_i2c_read(dev, reg, (uint8_t *) val, 4);
+static int rei2c_rd32(struct rei2c_dev_s *dev, uint8_t reg, uint32_t * val)
+{
+  return rei2c_i2c_read(dev, reg, (uint8_t *) val, 4);
 }
 
-static int rei2c_wr8(struct rei2c_dev_s *dev, uint8_t reg, uint8_t val) {
-	return rei2c_i2c_write(dev, reg, &val, 1);
+static int rei2c_wr8(struct rei2c_dev_s *dev, uint8_t reg, uint8_t val)
+{
+  return rei2c_i2c_write(dev, reg, &val, 1);
 }
 
-static int rei2c_wr24(struct rei2c_dev_s *dev, uint8_t reg, uint32_t val) {
-	return rei2c_i2c_write(dev, reg, (uint8_t *) & val, 3);
+static int rei2c_wr24(struct rei2c_dev_s *dev, uint8_t reg, uint32_t val)
+{
+  return rei2c_i2c_write(dev, reg, (uint8_t *) & val, 3);
 }
 
-static int rei2c_wr32(struct rei2c_dev_s *dev, uint8_t reg, uint32_t val) {
-	return rei2c_i2c_write(dev, reg, (uint8_t *) & val, 4);
-}
-
-/****************************************************************************/
-
-static inline int rei2c_takesem(sem_t * sem) {
-	int rc;
-	// Take a count from the semaphore, possibly waiting */
-	rc = nxsem_wait(sem);
-	// The only case that an error should occur here is if the wait was awakened by a signal
-	DEBUGASSERT(rc == OK || rc == -EINTR);
-	return rc;
-}
-
-static inline int rei2c_givesem(sem_t * sem) {
-	return nxsem_post(sem);
+static int rei2c_wr32(struct rei2c_dev_s *dev, uint8_t reg, uint32_t val)
+{
+  return rei2c_i2c_write(dev, reg, (uint8_t *) & val, 4);
 }
 
 /****************************************************************************/
 
-static int rei2c_init(struct rei2c_dev_s *dev, const struct rei2c_regs *regs) {
-	int rc;
+static inline int rei2c_takesem(sem_t * sem)
+{
+  int rc;
+  // Take a count from the semaphore, possibly waiting */
+  rc = nxsem_wait(sem);
+  // The only case that an error should occur here is if the wait was awakened by a signal
+  DEBUGASSERT(rc == OK || rc == -EINTR);
+  return rc;
+}
 
-	rc = rei2c_wr8(dev, REI2C_GCONF, REI2C_GCONF_RESET);
-	if (rc < 0) {
-		ierr("device reset failed %d\n", rc);
-		goto exit;
-	}
-	// wait > 400 usecs
-	nxsig_usleep(800);
+static inline int rei2c_givesem(sem_t * sem)
+{
+  return nxsem_post(sem);
+}
 
-	// check some register values
-	uint8_t val0, val1;
-	rei2c_rd8(dev, REI2C_GP1CONF, &val0);
-	rei2c_rd8(dev, REI2C_ANTBOUNC, &val1);
-	if ((val0 != 0) || (val1 != 25)) {
-		ierr("bad device values\n");
-		rc = -1;
-		goto exit;
-	}
-	// apply the user register configuration
-	if (regs != NULL) {
-		int idx = 0;
-		while (regs[idx].reg != 0xff) {
-			uint8_t reg = regs[idx].reg;
-			uint32_t val = regs[idx].val;
-			if ((reg == REI2C_CVAL) || (reg == REI2C_CMAX) || (reg == REI2C_CMIN) || (reg == REI2C_ISTEP)) {
-				rei2c_wr32(dev, reg, val);
-			} else {
-				rei2c_wr8(dev, reg, (uint8_t) val);
-			}
-			idx += 1;
-		}
-	}
+/****************************************************************************/
 
- exit:
-	return rc;
+static int rei2c_init(struct rei2c_dev_s *dev, const struct rei2c_regs *regs)
+{
+  int rc;
+
+  rc = rei2c_wr8(dev, REI2C_GCONF, REI2C_GCONF_RESET);
+  if (rc < 0)
+    {
+      ierr("device reset failed %d\n", rc);
+      goto exit;
+    }
+  // wait > 400 usecs
+  nxsig_usleep(800);
+
+  // check some register values
+  uint8_t val0, val1;
+  rei2c_rd8(dev, REI2C_GP1CONF, &val0);
+  rei2c_rd8(dev, REI2C_ANTBOUNC, &val1);
+  if ((val0 != 0) || (val1 != 25))
+    {
+      ierr("bad device values\n");
+      rc = -1;
+      goto exit;
+    }
+  // apply the user register configuration
+  if (regs != NULL)
+    {
+      int idx = 0;
+      while (regs[idx].reg != 0xff)
+        {
+          uint8_t reg = regs[idx].reg;
+          uint32_t val = regs[idx].val;
+          if ((reg == REI2C_CVAL) || (reg == REI2C_CMAX) ||
+              (reg == REI2C_CMIN) || (reg == REI2C_ISTEP))
+            {
+              rei2c_wr32(dev, reg, val);
+            }
+          else
+            {
+              rei2c_wr8(dev, reg, (uint8_t) val);
+            }
+          idx += 1;
+        }
+    }
+
+exit:
+  return rc;
 }
 
 /****************************************************************************/
 // file operations
 
-static int rei2c_open(struct file *filep) {
-	iinfo("filep %p\n", filep);
+static int rei2c_open(struct file *filep)
+{
+  iinfo("filep %p\n", filep);
 
-	DEBUGASSERT(filep && filep->f_inode);
-	struct inode *inode = filep->f_inode;
-	DEBUGASSERT(inode->i_private);
-	struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
+  DEBUGASSERT(filep && filep->f_inode);
+  struct inode *inode = filep->f_inode;
+  DEBUGASSERT(inode->i_private);
+  struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
 
-	int ret = rei2c_takesem(&dev->sem);
-	if (ret < 0) {
-		ierr("ERROR: rei2c_takesem failed: %d\n", ret);
-		return ret;
-	}
-	// Increment the reference count
-	uint8_t tmp = dev->crefs + 1;
-	if (tmp == 0) {
-		// More than 255 opens; uint8_t overflows to zero
-		ret = -EMFILE;
-		goto exit;
-	}
-	dev->crefs = tmp;
+  int ret = rei2c_takesem(&dev->sem);
+  if (ret < 0)
+    {
+      ierr("ERROR: rei2c_takesem failed: %d\n", ret);
+      return ret;
+    }
+  // Increment the reference count
+  uint8_t tmp = dev->crefs + 1;
+  if (tmp == 0)
+    {
+      // More than 255 opens; uint8_t overflows to zero
+      ret = -EMFILE;
+      goto exit;
+    }
+  dev->crefs = tmp;
 
- exit:
-	rei2c_givesem(&dev->sem);
-	return ret;
+exit:
+  rei2c_givesem(&dev->sem);
+  return ret;
 }
 
-static int rei2c_close(struct file *filep) {
-	iinfo("filep %p\n", filep);
+static int rei2c_close(struct file *filep)
+{
+  iinfo("filep %p\n", filep);
 
-	DEBUGASSERT(filep && filep->f_inode);
-	struct inode *inode = filep->f_inode;
-	DEBUGASSERT(inode->i_private);
-	struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
+  DEBUGASSERT(filep && filep->f_inode);
+  struct inode *inode = filep->f_inode;
+  DEBUGASSERT(inode->i_private);
+  struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
 
-	int ret = rei2c_takesem(&dev->sem);
-	if (ret < 0) {
-		ierr("ERROR: rei2c_takesem failed: %d\n", ret);
-		return ret;
-	}
-	// Decrement the reference count
-	if (dev->crefs >= 1) {
-		dev->crefs--;
-	}
+  int ret = rei2c_takesem(&dev->sem);
+  if (ret < 0)
+    {
+      ierr("ERROR: rei2c_takesem failed: %d\n", ret);
+      return ret;
+    }
+  // Decrement the reference count
+  if (dev->crefs >= 1)
+    {
+      dev->crefs--;
+    }
 
-	rei2c_givesem(&dev->sem);
-	return ret;
-}
-
-/****************************************************************************/
-
-static int rei2c_rd_status(struct rei2c_dev_s *dev, struct rei2c_status *status) {
-	uint8_t buf[7];
-	int ret = rei2c_i2c_read(dev, REI2C_ESTATUS, buf, sizeof(buf));
-	if (ret < 0) {
-		goto exit;
-	}
-	status->enc = buf[0];
-	status->int2 = buf[1];
-	status->fade = buf[2];
-	status->cnt = *(uint32_t *) & buf[3];
- exit:
-	return ret;
-}
-
-static ssize_t rei2c_read(struct file *filep, char *buffer, size_t buflen) {
-	iinfo("filep %p buffer %p buflen %d\n", filep, buffer, buflen);
-
-	DEBUGASSERT(filep && filep->f_inode);
-	struct inode *inode = filep->f_inode;
-	DEBUGASSERT(inode->i_private);
-	struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
-
-	if (buflen < sizeof(struct rei2c_status)) {
-		ierr("read buffer is too small %d < %d\n", buflen, sizeof(struct rei2c_status));
-		return -ENOSYS;
-	}
-
-	int ret = rei2c_takesem(&dev->sem);
-	if (ret < 0) {
-		ierr("ERROR: rei2c_takesem failed: %d\n", ret);
-		return ret;
-	}
-	// read the status value
-	struct rei2c_status *status = (struct rei2c_status *)buffer;
-	ret = rei2c_rd_status(dev, status);
-	if (ret >= 0) {
-		iinfo("enc %02x int2 %02x fade %02x cnt %08x\n", status->enc, status->int2, status->fade, status->cnt);
-		ret = sizeof(struct rei2c_status);
-	}
-
-	rei2c_givesem(&dev->sem);
-	return ret;
+  rei2c_givesem(&dev->sem);
+  return ret;
 }
 
 /****************************************************************************/
 
-static ssize_t rei2c_write(struct file *filep, const char *buffer, size_t buflen) {
-	iinfo("filep %p buffer %p buflen %d\n", filep, buffer, buflen);
-	return buflen;
+static int rei2c_rd_status(struct rei2c_dev_s *dev, struct rei2c_status *status)
+{
+  uint8_t buf[7];
+  int ret = rei2c_i2c_read(dev, REI2C_ESTATUS, buf, sizeof(buf));
+  if (ret < 0)
+    {
+      goto exit;
+    }
+  status->enc = buf[0];
+  status->int2 = buf[1];
+  status->fade = buf[2];
+  status->cnt = *(uint32_t *) & buf[3];
+exit:
+  return ret;
 }
 
-static int rei2c_ioctl(struct file *filep, int cmd, unsigned long arg) {
-	iinfo("filep %p cmd %d arg %08x\n", filep, cmd, arg);
-	return OK;
+static ssize_t rei2c_read(struct file *filep, char *buffer, size_t buflen)
+{
+  iinfo("filep %p buffer %p buflen %d\n", filep, buffer, buflen);
+
+  DEBUGASSERT(filep && filep->f_inode);
+  struct inode *inode = filep->f_inode;
+  DEBUGASSERT(inode->i_private);
+  struct rei2c_dev_s *dev = (struct rei2c_dev_s *)inode->i_private;
+
+  if (buflen < sizeof(struct rei2c_status))
+    {
+      ierr("read buffer is too small %d < %d\n", buflen,
+           sizeof(struct rei2c_status));
+      return -ENOSYS;
+    }
+
+  int ret = rei2c_takesem(&dev->sem);
+  if (ret < 0)
+    {
+      ierr("ERROR: rei2c_takesem failed: %d\n", ret);
+      return ret;
+    }
+  // read the status value
+  struct rei2c_status *status = (struct rei2c_status *)buffer;
+  ret = rei2c_rd_status(dev, status);
+  if (ret >= 0)
+    {
+      iinfo("enc %02x int2 %02x fade %02x cnt %08x\n", status->enc,
+            status->int2, status->fade, status->cnt);
+      ret = sizeof(struct rei2c_status);
+    }
+
+  rei2c_givesem(&dev->sem);
+  return ret;
+}
+
+/****************************************************************************/
+
+static ssize_t rei2c_write(struct file *filep, const char *buffer,
+                           size_t buflen)
+{
+  iinfo("filep %p buffer %p buflen %d\n", filep, buffer, buflen);
+  return buflen;
+}
+
+static int rei2c_ioctl(struct file *filep, int cmd, unsigned long arg)
+{
+  iinfo("filep %p cmd %d arg %08x\n", filep, cmd, arg);
+  return OK;
 }
 
 #ifndef CONFIG_DISABLE_POLL
-static int rei2c_poll(struct file *filep, struct pollfd *fds, bool setup) {
-	iinfo("filep %p fds %p setup %d\n", filep, fds, setup);
-	return OK;
+static int rei2c_poll(struct file *filep, struct pollfd *fds, bool setup)
+{
+  iinfo("filep %p fds %p setup %d\n", filep, fds, setup);
+  return OK;
 }
-#endif				// CONFIG_DISABLE_POLL
+#endif // CONFIG_DISABLE_POLL
 
 static const struct file_operations rei2c_fops = {
-	rei2c_open,		// open
-	rei2c_close,		// close
-	rei2c_read,		// read
-	rei2c_write,		// write
-	NULL,			// seek
-	rei2c_ioctl,		// ioctl
+  rei2c_open,                   // open
+  rei2c_close,                  // close
+  rei2c_read,                   // read
+  rei2c_write,                  // write
+  NULL,                         // seek
+  rei2c_ioctl,                  // ioctl
 #ifndef CONFIG_DISABLE_POLL
-	rei2c_poll,		// poll
+  rei2c_poll,                   // poll
 #endif
 #ifndef CONFIG_DISABLE_PSEUDOFS_OPERATIONS
-	NULL,			// unlink
+  NULL,                         // unlink
 #endif
 };
 
 /****************************************************************************/
 
-int rei2c_register(const char *devname, struct i2c_master_s *i2c, const struct rei2c_cfg *cfg) {
-	struct rei2c_dev_s *dev;
-	int rc;
+int rei2c_register(const char *devname, struct i2c_master_s *i2c,
+                   const struct rei2c_cfg *cfg)
+{
+  struct rei2c_dev_s *dev;
+  int rc;
 
-	iinfo("%s\n", devname);
+  iinfo("%s\n", devname);
 
-	DEBUGASSERT(devname && i2c && cfg);
+  DEBUGASSERT(devname && i2c && cfg);
 
-	// Allocate a new rei2c driver instance
-	dev = (struct rei2c_dev_s *)kmm_zalloc(sizeof(struct rei2c_dev_s));
-	if (!dev) {
-		ierr("failed to allocate device structure\n");
-		return -ENOMEM;
-	}
-	// setup the device structure
-	dev->i2c = i2c;
-	dev->speed = cfg->speed;
-	dev->addr = cfg->addr;
-	nxsem_init(&dev->sem, 0, 1);
+  // Allocate a new rei2c driver instance
+  dev = (struct rei2c_dev_s *)kmm_zalloc(sizeof(struct rei2c_dev_s));
+  if (!dev)
+    {
+      ierr("failed to allocate device structure\n");
+      return -ENOMEM;
+    }
+  // setup the device structure
+  dev->i2c = i2c;
+  dev->speed = cfg->speed;
+  dev->addr = cfg->addr;
+  nxsem_init(&dev->sem, 0, 1);
 
-	// initialize the hardware
-	rc = rei2c_init(dev, cfg->regs);
-	if (rc < 0) {
-		ierr("rei2c_init failed %d\n", rc);
-		goto exit;
-	}
-	// register the rei2c driver
-	rc = register_driver(devname, &rei2c_fops, 0666, dev);
-	if (rc < 0) {
-		ierr("register_driver failed %d\n", rc);
-		goto exit;
-	}
+  // initialize the hardware
+  rc = rei2c_init(dev, cfg->regs);
+  if (rc < 0)
+    {
+      ierr("rei2c_init failed %d\n", rc);
+      goto exit;
+    }
+  // register the rei2c driver
+  rc = register_driver(devname, &rei2c_fops, 0666, dev);
+  if (rc < 0)
+    {
+      ierr("register_driver failed %d\n", rc);
+      goto exit;
+    }
 
-	return OK;
+  return OK;
 
- exit:
-	nxsem_destroy(&dev->sem);
-	kmm_free(dev);
-	return rc;
+exit:
+  nxsem_destroy(&dev->sem);
+  kmm_free(dev);
+  return rc;
 }
 
 /****************************************************************************/
--- files/nuttx/include/nuttx/input/rei2c.h
+++ files/nuttx/include/nuttx/input/rei2c.h
@@ -36,74 +36,72 @@
 #ifndef __INCLUDE_NUTTX_INPUT_REI2C_H
 #define __INCLUDE_NUTTX_INPUT_REI2C_H
 
-/****************************************************************************
- * Included Files
- ****************************************************************************/
+/****************************************************************************/
 
 #include <nuttx/config.h>
 #include <nuttx/input/ioctl.h>
 #include <nuttx/i2c/i2c_master.h>
 
-/* Registers Addresses ******************************************************/
+/****************************************************************************/
 
-#define REI2C_GCONF             0x00    /* General Configuration (1 byte) */
-#define REI2C_GP1CONF           0x01    /* GP 1 Configuration (1 byte) */
-#define REI2C_GP2CONF           0x02    /* GP 2 Configuration (1 byte) */
-#define REI2C_GP3CONF           0x03    /* GP 3 Configuration (1 byte) */
-#define REI2C_INTCONF           0x04    /* INT pin Configuration (1 byte) */
-#define REI2C_ESTATUS           0x05    /* Encoder Status (1 byte) */
-#define REI2C_I2STATUS          0x06    /* Secondary interrupt status (1 byte) */
-#define REI2C_FSTATUS           0x07    /* Fade process status (1 byte) */
-#define REI2C_CVAL              0x08    /* Counter Value (4 bytes) */
-#define REI2C_CMAX              0x0C    /* Counter Max value (4 bytes) */
-#define REI2C_CMIN              0x10    /* Counter Min value (4 bytes) */
-#define REI2C_ISTEP             0x14    /* Increment step value (4 bytes) */
-#define REI2C_RLED              0x18    /* LED red color intensity (1 byte) */
-#define REI2C_GLED              0x19    /* LED green color intensity (1 byte) */
-#define REI2C_BLED              0x1A    /* LED blue color intensity (1 byte) */
-#define REI2C_GP1REG            0x1B    /* I/O GP1 Register (1 byte) */
-#define REI2C_GP2REG            0x1C    /* I/O GP2 Register (1 byte) */
-#define REI2C_GP3REG            0x1D    /* I/O GP3 Register (1 byte) */
-#define REI2C_ANTBOUNC          0x1E    /* Anti-bouncing period (1 Byte) */
-#define REI2C_DPPERIOD          0x1F    /* Double push period (1 Byte) */
-#define REI2C_FADERGB           0x20    /* Fade timer RGB Encoder (1 Byte) */
-#define REI2C_FADEGP            0x21    /* Fade timer GP ports (1 Byte) */
-#define REI2C_EEPROM            0x80    /* EEPROM memory (128 bytes) */
+/* Register Addresses */
+
+#define REI2C_GCONF      0x00   /* General Configuration (1 byte) */
+#define REI2C_GP1CONF    0x01   /* GP 1 Configuration (1 byte) */
+#define REI2C_GP2CONF    0x02   /* GP 2 Configuration (1 byte) */
+#define REI2C_GP3CONF    0x03   /* GP 3 Configuration (1 byte) */
+#define REI2C_INTCONF    0x04   /* INT pin Configuration (1 byte) */
+#define REI2C_ESTATUS    0x05   /* Encoder Status (1 byte) */
+#define REI2C_I2STATUS   0x06   /* Secondary interrupt status (1 byte) */
+#define REI2C_FSTATUS    0x07   /* Fade process status (1 byte) */
+#define REI2C_CVAL       0x08   /* Counter Value (4 bytes) */
+#define REI2C_CMAX       0x0C   /* Counter Max value (4 bytes) */
+#define REI2C_CMIN       0x10   /* Counter Min value (4 bytes) */
+#define REI2C_ISTEP      0x14   /* Increment step value (4 bytes) */
+#define REI2C_RLED       0x18   /* LED red color intensity (1 byte) */
+#define REI2C_GLED       0x19   /* LED green color intensity (1 byte) */
+#define REI2C_BLED       0x1A   /* LED blue color intensity (1 byte) */
+#define REI2C_GP1REG     0x1B   /* I/O GP1 Register (1 byte) */
+#define REI2C_GP2REG     0x1C   /* I/O GP2 Register (1 byte) */
+#define REI2C_GP3REG     0x1D   /* I/O GP3 Register (1 byte) */
+#define REI2C_ANTBOUNC   0x1E   /* Anti-bouncing period (1 Byte) */
+#define REI2C_DPPERIOD   0x1F   /* Double push period (1 Byte) */
+#define REI2C_FADERGB    0x20   /* Fade timer RGB Encoder (1 Byte) */
+#define REI2C_FADEGP     0x21   /* Fade timer GP ports (1 Byte) */
+#define REI2C_EEPROM     0x80   /* EEPROM memory (128 bytes) */
 
 /* REI2C_GCONF bits */
 
-#define REI2C_GCONF_DTYPE       (1 << 0)    /* Data type of the register: CVAL, CMAX, CMIN and ISTEP. */
-#define REI2C_GCONF_WRAPE       (1 << 1)    /* Enable counter wrap. */
-#define REI2C_GCONF_DIRE        (1 << 2)    /* Direction of the encoder when increment. */
-#define REI2C_GCONF_IPUD        (1 << 3)    /* Interrupt Pull-UP disable. */
-#define REI2C_GCONF_RMOD        (1 << 4)    /* Reading Mode. */
-#define REI2C_GCONF_ETYPE       (1 << 5)    /* Set the encoder type (normal/illuminated) */
-#define REI2C_GCONF_MBANK       (1 << 6)    /* Select the EEPROM memory bank. Each bank is 128 bytes. */
-#define REI2C_GCONF_RESET       (1 << 7)    /* Reset the I2C Encoder V2 */
+#define REI2C_GCONF_DTYPE (1 << 0)      /* Data type of the register: CVAL, CMAX, CMIN and ISTEP. */
+#define REI2C_GCONF_WRAPE (1 << 1)      /* Enable counter wrap. */
+#define REI2C_GCONF_DIRE  (1 << 2)      /* Direction of the encoder when increment. */
+#define REI2C_GCONF_IPUD  (1 << 3)      /* Interrupt Pull-UP disable. */
+#define REI2C_GCONF_RMOD  (1 << 4)      /* Reading Mode. */
+#define REI2C_GCONF_ETYPE (1 << 5)      /* Set the encoder type (normal/illuminated) */
+#define REI2C_GCONF_MBANK (1 << 6)      /* Select the EEPROM memory bank. Each bank is 128 bytes. */
+#define REI2C_GCONF_RESET (1 << 7)      /* Reset the I2C Encoder V2 */
 
 /* REI2C_ESTATUS bits */
 
-#define REI2C_ESTATUS_PUSHR     (1 << 0)    /* push button has been released */
-#define REI2C_ESTATUS_PUSHP     (1 << 1)    /* push button has been pressed */
-#define REI2C_ESTATUS_PUSHD     (1 << 2)    /* push button has been double pushed */
-#define REI2C_ESTATUS_RINC      (1 << 3)    /* rotated in the increase direction */
-#define REI2C_ESTATUS_RDEC      (1 << 4)    /* rotated in the decrease direction */
-#define REI2C_ESTATUS_RMAX      (1 << 5)    /* maximum counter value has been reached */
-#define REI2C_ESTATUS_RMIN      (1 << 6)    /* minimum counter value has been reached */
-#define REI2C_ESTATUS_INT2      (1 << 7)    /* Secondary interrupt status */
+#define REI2C_ESTATUS_PUSHR (1 << 0)    /* push button has been released */
+#define REI2C_ESTATUS_PUSHP (1 << 1)    /* push button has been pressed */
+#define REI2C_ESTATUS_PUSHD (1 << 2)    /* push button has been double pushed */
+#define REI2C_ESTATUS_RINC  (1 << 3)    /* rotated in the increase direction */
+#define REI2C_ESTATUS_RDEC  (1 << 4)    /* rotated in the decrease direction */
+#define REI2C_ESTATUS_RMAX  (1 << 5)    /* maximum counter value has been reached */
+#define REI2C_ESTATUS_RMIN  (1 << 6)    /* minimum counter value has been reached */
+#define REI2C_ESTATUS_INT2  (1 << 7)    /* Secondary interrupt status */
 
-/****************************************************************************
- * Public Types
- ****************************************************************************/
+/****************************************************************************/
 
 /* status returned by rei2c_read operation */
 
 struct rei2c_status
 {
-  uint8_t enc;          /* Encoder Status */
-  uint8_t int2;         /* Secondary interrupt status */
-  uint8_t fade;         /* Fade process status */
-  uint32_t cnt;         /* Counter Value */
+  uint8_t enc;                  /* Encoder Status */
+  uint8_t int2;                 /* Secondary interrupt status */
+  uint8_t fade;                 /* Fade process status */
+  uint32_t cnt;                 /* Counter Value */
 };
 
 /* rei2c user provided register values */
@@ -118,16 +116,18 @@ struct rei2c_regs
 
 struct rei2c_cfg
 {
-  uint32_t speed;                   /* i2c bus speed */
-  uint8_t addr;                     /* i2c device address */
-  const struct rei2c_regs *regs;    /* user provided register values */
+  uint32_t speed;               /* i2c bus speed */
+  uint8_t addr;                 /* i2c device address */
+  const struct rei2c_regs *regs;        /* user provided register values */
 };
 
-/****************************************************************************
- * Public Function Prototypes
- ****************************************************************************/
+/****************************************************************************/
 
 int rei2c_register(const char *devname, struct i2c_master_s *i2c,
                    const struct rei2c_cfg *cfg);
 
+/****************************************************************************/
+
 #endif /* __INCLUDE_NUTTX_INPUT_REI2C_H */
+
+/****************************************************************************/
