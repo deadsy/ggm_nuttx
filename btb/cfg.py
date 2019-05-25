72c1be3276d6148da4f945c10eb449f5924b93de
Jason Harris <sirmanlypowers@gmail.com>
2019-05-24 18:45:52 -0700

    WIP. build script to regression test code changes on other platforms.

---
+++ btb/cfg.py
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+"""
+
+BtB configuration
+
+"""
+#------------------------------------------------------------------------------
+
+# root path
+root_path = '/home/jasonh/work/jasonh/ggm'
+
+# path to nuttx source
+nuttx_path = root_path + '/src/nuttx'
+
+# path to apps source
+apps_path = root_path + '/src/apps'
+
+#------------------------------------------------------------------------------
