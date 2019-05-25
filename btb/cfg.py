1ff9bb0878d8bdc127c7c8f77f2adeb0847663b2
Jason Harris <sirmanlypowers@gmail.com>
2019-05-25 12:32:38 -0700

    First working btb script.

--- btb/cfg.py
+++ btb/cfg.py
@@ -6,13 +6,28 @@ BtB configuration
 """
 #------------------------------------------------------------------------------
 
-# root path
-root_path = '/home/jasonh/work/jasonh/ggm'
+# base path
+base_path = '/home/jasonh/work/ggm'
 
-# path to nuttx source
-nuttx_path = root_path + '/src/nuttx'
+# build path
+build_path = base_path + '/build'
 
-# path to apps source
-apps_path = root_path + '/src/apps'
+# download archive path
+archive_path = base_path + '/dl'
+
+# build status path
+status_path = base_path + '/status'
+
+# path to cross compilation tools
+xtools = '/opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-'
+
+# git hash of nuttx source
+nuttx_hash = '52203c4ae158c94ecaa93aa3a3bbb9a82fb5eeb9'
+
+# git hash of apps source
+apps_hash = '891ea23356f4542035f5ba00b84a0c48a7cdad4f'
+
+debug = False
+#debug = True
 
 #------------------------------------------------------------------------------
