48cdf097ed19462e8ce044a49831e9e979566149
Jason Harris <sirmanlypowers@gmail.com>
2019-05-27 08:43:54 -0700

    Updates to build script.

--- btb/cfg.py
+++ btb/cfg.py
@@ -22,12 +22,17 @@ status_path = base_path + '/status'
 xtools = '/opt/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-'
 
 # git hash of nuttx source
-nuttx_hash = '52203c4ae158c94ecaa93aa3a3bbb9a82fb5eeb9'
+#nuttx_hash = '52203c4ae158c94ecaa93aa3a3bbb9a82fb5eeb9'
+nuttx_hash = 'fmc'
 
 # git hash of apps source
 apps_hash = '891ea23356f4542035f5ba00b84a0c48a7cdad4f'
 
+# debug flag
 debug = False
 #debug = True
+
+# number of log lines added to the bad build file
+log_lines = 20
 
 #------------------------------------------------------------------------------
